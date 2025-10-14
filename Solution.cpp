2c23e5509811a5773fc5ad3374ea2630c642fdb2
bytezen-cmd <bytepulse2025@outlook.com>
2025-10-14 18:35:22 +0530

    Update 1

--- Solution.cpp
+++ Solution.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
 class Solution {
 private:
 public:
+    int findMiddleIndex(std::vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int sumLeft = std::accumulate(nums.begin(), nums.begin() + i, 0);
+            int sumRight = std::accumulate(nums.begin() + i + 1, nums.begin() + nums.size(), 0);
+            if (sumLeft == sumRight)
+                return i;
+        }
+        return -1;
+    }
 
 };
