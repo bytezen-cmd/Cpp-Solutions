ea08ebf3ce5a1cf373a37e2853f065b4de4517ae
bytezen-cmd <bytepulse2025@outlook.com>
2025-10-14 19:07:20 +0530

    Added Methods

--- Solution.cpp
+++ Solution.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <string>
+#include <cstdlib>
 
 class Solution {
 private:
@@ -15,4 +17,39 @@ public:
         return -1;
     }
 
+    std::string interpret(std::string command) {
+        std::string returner = "";
+        for (int x = 0; x < command.length(); x++) {
+            if (command[x] == 'G')
+                returner += "G";
+            if (command.substr(x, 2) == "()")
+                returner += "o";
+            if (command.substr(x, 4) == "(al)")
+                returner += "al";
+        }
+        return returner;
+    }
+
+    int countGoodTriplets(std::vector<int>& arr, int a, int b, int c) {
+        int count{ 0 };
+
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                for (int k = j + 1; k < arr.size(); k++) {
+                    if (std::abs(arr[i] - arr[j]) <= a && std::abs(arr[j] - arr[k]) <= b && std::abs(arr[i] - arr[k]) <= c) {
+                        count += 1;
+                    }
+
+                }
+            }
+        }
+
+        return count;
+    }
+
 };
+
+int main() 
+{
+
+}
