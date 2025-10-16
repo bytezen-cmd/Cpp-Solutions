264a48896f72a8cdf0621a97985b11457a7046a1
bytezen-cmd <bytepulse2025@outlook.com>
2025-10-16 21:26:19 +0530

    Another Try

--- codeforces-watermelon.cpp
+++ codeforces-watermelon.cpp
@@ -3,14 +3,14 @@
 int main() {
 	int w;
 	std::cin >> w;
-
-	if (w % 2 == 0) {
-		std::cout << true;
-	}
-	else {
-		std::cout << false;
-	}
-
+	bool condition;
+	for (int i = 0; i <= w; i++)
+		if (i % 2 == 0 && (w - i) % 2 == 0)
+			condition = true;
+	if (condition)
+		std::cout << "YES";
+	else
+		std::cout << "NO";
 	return 0;
 
 }
