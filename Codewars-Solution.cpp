5309bf3c625c660bb742179f5d6f5191c6028fd1
bytezen-cmd <bytepulse2025@outlook.com>
2025-10-19 21:28:41 +0530

    Added New Methods

--- Codewars-Solution.cpp
+++ Codewars-Solution.cpp
@@ -1,8 +1,29 @@
 #include <string>
 #include <cctype>
 #include <iostream>
+#include <cmath>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
 
 namespace CodewarsSolution {
+
+    bool is_pangram(const std::string& s) {
+        int charCount[26] = { 0 };
+
+        for (char c : s) {
+            if (std::isalpha(c)) {
+                c = std::tolower(c);
+                charCount[c - 'a'] += 1;
+            }
+        }
+
+        for (int j = 0; j < 26; j++)
+            if (charCount[j] < 1)
+                return false;
+
+        return true;
+    }
 
     std::string rot13(std::string msg)
     {
@@ -15,5 +36,71 @@ namespace CodewarsSolution {
         }
         return msg;
     }
+
+    bool narcissistic(int value) {
+        std::stringstream temp;
+        temp << value;
+        std::string s = temp.str();
+        int length = s.length();
+        int sum{ 0 };
+        for (int i = 0; i < length; i++) {
+            sum += std::pow(((int)s[i] - '0'), length);
+        }
+        return (sum == value);
+    }
+
+    std::size_t duplicateCount(const std::string& in)
+    {
+        std::unordered_map<char, int> characterCount;
+        for (char c : in) {
+            c = std::tolower(c);
+            if (characterCount.find(c) != characterCount.end())
+                characterCount[c] += 1;
+            else
+                characterCount[c] = 1;
+        }
+        int count{ 0 };
+        for (auto& pair : characterCount)
+            if (characterCount[pair.first] > 1)
+                count += 1;
+        return count;
+    }
+
+    class LongestConsec
+    {
+    public:
+        static std::string longestConsec(const std::vector<std::string>& strarr, int k)
+        {
+            if (k <= 0 || k > strarr.size()) return "";
+
+            std::string current = "";
+            size_t currLength{ 0UL };
+            for (size_t i = 0; i < strarr.size() - k + 1; i++) {
+                std::string temp;
+                for (size_t j = i; j < k + i; j++)
+                    temp += strarr[j];
+                if (temp.length() > currLength) {
+                    current = temp;
+                    currLength = temp.length();
+                }
+            }
+            return current;
+
+        }
+
+    };
+
+    std::vector<std::string> wave(std::string y) {
+        std::vector<std::string> waveSequence;
+        for (int i = 0; i < y.size(); i++) {
+            std::string temp = y;
+            if (temp[i] != ' ') {
+                temp[i] = temp[i] - 32;
+                waveSequence.push_back(temp);
+            }
+        }
+        return waveSequence;
+    }
+
 };
 
